7a5f1b10b525d8dccf33615da435a1af74769752
Mario Filgueiras <magallanesdeveloper@gmail.com>
2021-06-20 23:16:38 +0200

    Botón crea registro funcionando

--- mycrypto/static/js/mycrypto.js
+++ mycrypto/static/js/mycrypto.js
@@ -1,33 +1,33 @@
-let losMovimientos
+const movGlobal = {}
 
 xhr = new XMLHttpRequest()
 
 
 function recibeRespuestaCoinmarket() {
+    
     if (this.readyState === 4 && (this.status === 200 || this.status === 201)) {
         const respuesta = JSON.parse(this.responseText)
-
+        
         if (respuesta.status.error_message !== null) {
             alert("Se ha producido un error en acceso a Coin Market: " + respuesta.status.error_message)
             return
         }
         
-        const movimiento = {}
+        movGlobal.date = respuesta.status.timestamp.substring(0, 10)
+        console.log(movGlobal.date)
+        movGlobal.time = respuesta.status.timestamp.substring(11, 23)
+        console.log(movGlobal.time)
+        movGlobal.moneda_from = document.querySelector("#moneda_from").value
+        console.log(movGlobal.moneda_from)
+        movGlobal.cantidad_from = document.querySelector("#cantidad_from").value
+        console.log(movGlobal.cantidad_from)
+        movGlobal.moneda_to = document.querySelector("#moneda_to").value
+        console.log(movGlobal.moneda_to)
+        movGlobal.cantidad_to = respuesta.data.quote[movGlobal.moneda_to].price
+        console.log(movGlobal.cantidad_to)
+        document.querySelector("#cantidad_to").setAttribute("placeholder", movGlobal.cantidad_to)
 
-        movimiento.date = respuesta.status.timestamp.substring(0, 10)
-        console.log(movimiento.date)
-        movimiento.time = respuesta.status.timestamp.substring(11, 23)
-        console.log(movimiento.time)
-        movimiento.moneda_from = document.querySelector("#moneda_from").value
-        console.log(movimiento.moneda_from)
-        movimiento.cantidad_from = document.querySelector("#cantidad_from").value
-        console.log(movimiento.cantidad_from)
-        movimiento.moneda_to = document.querySelector("#moneda_to").value
-        console.log(movimiento.moneda_to)
-        movimiento.cantidad_to = respuesta.data.quote[movimiento.moneda_to].price
-        console.log(movimiento.cantidad_to)
-        document.querySelector("#cantidad_to").setAttribute("placeholder", movimiento.cantidad_to)
-
+        console.log(movGlobal)
     }    
 }
 
@@ -58,7 +58,6 @@ function muestraMovimientos() {
             return
         }
 
-        losMovimientos = respuesta.movimientos
         const tbody = document.querySelector(".tabla-movimientos tbody")  // lo insertamos dentro de tbody (mirar spa.html)
         tbody.innerHTML = ""
         
@@ -99,38 +98,20 @@ function llamaApiCoinmarket(evento) {
     xhr.open('GET', `https://pro-api.coinmarketcap.com/v1/tools/price-conversion?amount=${movimiento.cantidad_from}&symbol=${movimiento.moneda_from}&convert=${movimiento.moneda_to}&CMC_PRO_API_KEY=43095178-7dd8-48a4-8b66-e799480ad420`)
     xhr.onload = recibeRespuestaCoinmarket
     xhr.send()
-    console.log("He lanzado petición")
-
+    console.log("He lanzado petición a Coin Market")
 }   
-
-
-
-function capturaFormMovimiento() {
-    const movimiento = {}
-    movimiento.date = document.querySelector("#date").value
-    movimiento.time = document.querySelector("#time").value
-    movimiento.moneda_from = document.querySelector("#moneda_from").value
-    movimiento.cantidad_from = document.querySelector("#cantidad_from").value
-    movimiento.moneda_to = document.querySelector("#moneda_to").value
-    movimiento.cantidad_to = document.querySelector("#cantidad_to").value
-   
-    return movimiento 
-}
 
 
 
 function llamaApiCreaMovimiento(evento) {
     evento.preventDefault()
 
-    const movimiento = capturaFormMovimiento()
-
     xhr.open("POST", `http://localhost:5000/api/v1/movimiento`, true)
     xhr.onload = recibeRespuestaCreamovimiento
 
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
 
-    xhr.send(JSON.stringify(movimiento))
-    
+    xhr.send(JSON.stringify(movGlobal))
 }
 
 
